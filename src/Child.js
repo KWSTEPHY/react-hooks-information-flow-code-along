3f9bd3d7860c1be8b404fc06a676e342d1585c3c
stephy <stephshiro35@gmail.com>
2024-04-30 15:23:00 +0300

    data flow from child to parent and saring of data between siblings

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
-}
+function Child({ onChangeColor, color }) {
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  };// new function in our Child component that invokes onChangeColor and passes in a random color as the argument
 
+
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
+}
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,23 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  //function to update child colour when child is clicked
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+
+    <Child color={childrenColor} onChangeColor={handleChangeColor} />
+    <Child color={childrenColor} onChangeColor={handleChangeColor} />
+{/* pass info from child to parent
+      <Child onChangeColor={handleChangeColor}  />
+      <Child onChangeColor={handleChangeColor}  /> */}
     </div>
   );
 }
